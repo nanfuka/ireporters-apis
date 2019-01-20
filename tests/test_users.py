a64b4b4d4d42dad013f33bcfa7364887ea8560c2
nanfuka <kalungi2k6@gmail.com>
2019-01-20 16:33:15 +0300

    Add more tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -61,7 +61,66 @@ class TestUsers(BaseTestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(data['error'], 'Enter lastname')
 
-    def test_signup_with_Iinvalid_lastname(self):
+    def test_login_with_no_invalid_phonenumber(self):
+        user = {"firstname": "frae",
+                "lastname": "fgfdhfg",
+                "othernames": "Nsubuga",
+                "email": "kalungi2k6@yahoo.com",
+                "phoneNumber": "dfgf",
+                "username": "nanfuka",
+                "isAdmin": "true",
+                "password": "secrets"
+                }
+        response = self.test_client.post('/api/v1/signup', json=user)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['error'], 'phoneNumber should be made up of numbers')
+
+    def test_login_with_no_phonenumber(self):
+        user = {"firstname": "frae",
+                "lastname": "fgfdhfg",
+                "othernames": "Nsubuga",
+                "email": "kalungi2k6@yahoo.com",
+                
+                "username": "nanfuka",
+                "isAdmin": "true",
+                "password": "secrets"
+                }
+        response = self.test_client.post('/api/v1/signup', json=user)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['error'], 'Enter phone number')
+
+    def test_signup_with_no_email(self):
+        user = {"firstname": "frae",
+                "lastname": "sfdrg",
+                "othernames": "Nsubuga",
+                "email": "kampala",
+                "PhoneNumber": 777777,
+                "username": "nanfuka",
+                "isAdmin": "true",
+                "password": "secrets"
+                }
+        response = self.test_client.post('/api/v1/signup', json=user)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['error'], 'Invalid email, it should be in this format; kals@gma.com')
+
+    def test_signup_with_not_email(self):
+        user = {"firstname": "frae",
+                "lastname": "sfdrg",
+                "othernames": "Nsubuga",
+               
+                "PhoneNumber": 777777,
+                "username": "nanfuka",
+                "isAdmin": "true",
+                "password": "secrets"
+                }
+        response = self.test_client.post('/api/v1/signup', json=user)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['error'], 'Enter Email')
+    def test_signup_with_invalid_lastname(self):
         user = {"firstname": "4546",
                 "lastname": 4564,
                 "othernames": "Nsubuga",
@@ -75,6 +134,51 @@ class TestUsers(BaseTestCase):
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(data['error'], 'lastname should be a string')
+
+    def test_signup_with_invalid_admin_status(self):
+        user = {"firstname": "frae",
+                "lastname": "sfdrg",
+                "othernames": "Nsubuga",
+                "email": "kampala@yahoo.com",
+                "phoneNumber": 777777,
+                "username": "nanfuka",
+                "isAdmin": "trues",
+                "password": "secrets"
+                }
+        response = self.test_client.post('/api/v1/signup', json=user)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['error'], 'IsAdmin should either be true or false')
+
+    def test_signup_with_weak_password(self):
+        user = {"firstname": "frae",
+                "lastname": "sfdrg",
+                "othernames": "Nsubuga",
+                "email": "kampala@yahoo.com",
+                "phoneNumber": 777777,
+                "username": "nanfuka",
+                "isAdmin": "true",
+                "password": "dfg"
+                }
+        response = self.test_client.post('/api/v1/signup', json=user)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['error'], 'Increase the strength of your password')
+
+    def test_signup_with_no_password(self):
+        user = {"firstname": "frae",
+                "lastname": "sfdrg",
+                "othernames": "Nsubuga",
+                "email": "kampala@yahoo.com",
+                "phoneNumber": 777777,
+                "username": "nanfuka",
+                "isAdmin": "true",
+             
+                }
+        response = self.test_client.post('/api/v1/signup', json=user)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['error'], 'Enter password')
 
     def test_login(self):
         """method for testing the login"""
